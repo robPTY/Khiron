d696d6b69a7efa6a885353f0af0b44a9927ed2d5
keysh18 <114112736+keysh18@users.noreply.github.com>
2024-03-04 22:58:04 -0600

    dependency fixes

--- khiron_app/App.js
+++ khiron_app/App.js
@@ -1,7 +1,8 @@
 import { StatusBar } from 'expo-status-bar';
 import { Button, StyleSheet, Text, View, Image } from 'react-native';
-import { createAppContainer, createSwitchNavigator } from 'react-navigation';
+import { createAppContainer, createSwitchNavigator } from '@react-navigation/native';
 import { createStackNavigator } from 'react-navigation-stack';
+
 import LoadingScreen from './screens/LoadingScreen';
 import HomeScreen from './screens/HomeScreen';
 
