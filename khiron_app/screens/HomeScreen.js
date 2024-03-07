38b40c277309ba9067c752dde5dea78d4e7d8ca7
keysh18 <114112736+keysh18@users.noreply.github.com>
2024-03-07 15:53:21 -0600

    Update HomeScreen.js

--- khiron_app/screens/HomeScreen.js
+++ khiron_app/screens/HomeScreen.js
@@ -2,17 +2,12 @@ import { View, Text, Stylesheet, Button } from "react-native";
 import {useNavigation} from "@react-navigation/native";
 
 export default function HomeScreen(){
-<<<<<<< Updated upstream
     const navigation = useNavigation;
-    return(
-        <View style={Stylesheet.container}>
-            <Text style={styles.text}>Home Screen</Text>
-=======
+    
     return(
         <View>
           <Text>Hi</Text>
   
->>>>>>> Stashed changes
         </View>
     );
 }
